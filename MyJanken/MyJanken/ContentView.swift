b4444c7ee0a12bb6668899f7d9bd15a45a668d2f
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-05-02 09:13:31 +0900

    じゃんけん

--- MyJanken/MyJanken/ContentView.swift
+++ MyJanken/MyJanken/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(.gu)
+                .resizable()
+                .scaledToFit()
+            Text("グー")
+            
+            Button {
+                print("タップされたよ！")
+            } label: {
+                Text("じゃんけんをする")
+            }
+            
         }
-        .padding()
     }
 }
 
