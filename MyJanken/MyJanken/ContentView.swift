fa5f0adb29a0511139b1e3f6fe4429c5820a2f83
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-05-02 15:53:29 +0900

    じゃんけんアプリ完成

--- MyJanken/MyJanken/ContentView.swift
+++ MyJanken/MyJanken/ContentView.swift
@@ -8,24 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var jankenNumber = 0
     var body: some View {
         
         VStack {
-            Image(.gu)
-                .resizable()
-                .scaledToFit()
-            Text("グー")
+            if jankenNumber == 0{
+                Spacer()
+                Text("これからじゃんけんをします！")
+            }else if jankenNumber == 1{
+                Image(.gu)
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                Text("グー")
+                    .padding(.bottom)
+            }else if jankenNumber == 2{
+                Image(.choki)
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                Text("チョキ")
+                    .padding(.bottom)
+            }else{
+                Image(.pa)
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                Text("パー")
+                    .padding(.bottom)
+            }
             
             Button {
-                print("タップされたよ！")
+                var newJankenNumber = 0
+                repeat{
+                    newJankenNumber = Int.random(in: 0...3)
+                }while jankenNumber == newJankenNumber
+                
+                jankenNumber = newJankenNumber
             } label: {
                 Text("じゃんけんをする")
+                    .padding(.bottom)
+                    .frame(maxWidth: .infinity)
+                    .frame(height:100)
+                    .font(.title)
+                    .background(.pink)
+                    .foregroundStyle(.white)
             }
             
         }
     }
 }
-
 #Preview {
     ContentView()
 }
