3de6f0b81cf937735f6b2757cd2ac3442488f524
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-05-06 14:57:49 +0900

    タイマーアプリ途中

---
+++ MyTimer/MyTimer/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  MyTimer
+//
+//  Created by 高橋直斗 on 2025/05/06.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
