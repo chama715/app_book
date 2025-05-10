02ac1fbea90f0a5375c77e6379c8803f5e034e70
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-05-10 19:14:12 +0900

    復習

--- 練習.playground/Contents.swift
+++ 練習.playground/Contents.swift
@@ -1,42 +1,4 @@
-//if文
-/*if true {
-    print("trueなのでprint")
+//for文
+for num in 1...5 {
+    print(num)
 }
-
-if false {
-    print("falseなのでprintされない")
-}
-*/
-/*let score = 81
-if score > 40 {
-    print("赤点回避")
-}
-if score > 60 && score < 80 {
-    print("まずまずの出来ですね")
-}
-if score > 80 {
-    print("上出来です！")
-}
-*/
-print("###############################")
-
-//if-else文
-
-/*let score = 30
-if score > 65 {
-    print("合格です！")
-} else {
-    print("もっと頑張りましょう！")
-}
-*/
-
-//if-else if-else文
-/*let score = 0
-if score > 80 {
-    print("エクセレント!!!")
-} else if  score > 60 && score < 80 {
-    print("まずまずです")
-} else {
-    print("不合格")
-}
-*/
